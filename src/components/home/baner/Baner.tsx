78e37040c5227076250fc74cd55aff728e288ba0
alimosavar <alimosavar1996@gmail.com>
2024-09-19 16:40:32 +0330

    add features to baner

--- src/components/home/baner/Baner.tsx
+++ src/components/home/baner/Baner.tsx
@@ -1,21 +1,20 @@
 const Baner = () => {
   return (
-    <div className="mb-8">
-      {/* Desktop Section */}
-      <div className="hidden sm:flex flex-col justify-center items-center gap-2">
-        <span className="flex gap-2 text-3xl">
-          <p className="bg-pink-500 text-white rounded-lg p-1">لوکس</p> ترین ها
-          را از ما بخواهید
-        </span>
-        <span className="text-3xl text-gray-400 border-b-2">
-          Luxury Shose Gallery
-        </span>
-      </div>
-
-      {/* Mobile Section */}
-      <div className="flex items-center justify-between sm:hidden">
-        Mobile Section
-      </div>
+    <div className="flex flex-col justify-center items-center gap-2">
+      <span className="flex gap-2 text-2xl sm:text-3xl">
+        <p className="bg-pink-500 text-white rounded-lg p-1">لوکس</p> ترین ها را
+        از ما بخواهید
+      </span>
+      <span className="text-gray-400 border-b-2 text-2xl sm:text-3xl">
+        Luxury Shose Gallery
+      </span>
+      <span className="w-1/2 text-center">
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده
+        از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
+        سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
+        متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه
+        درصد گذشته حال و آینده.
+      </span>
     </div>
   );
 };
